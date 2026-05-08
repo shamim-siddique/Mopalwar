87a5299919adf687f0d68a13f2009ec32bf18ba4
Shamim <siddique.mshamim@gmail.com>
2026-05-08 11:16:24 +0530

    add scrolltotop functionality

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import ScrollToTop from '../components/ScrollToTop'
 
 const Layout = () => {
   return (
     <div className="min-h-screen bg-cream-50">
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
